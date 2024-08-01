d98ccc8bde93e53608e190823cdc6a70c22223d4
AnnaHtslk <hannah.htslk@gmail.com>
2024-08-01 14:13:18 +0300

    fixed error with api link

--- src/config/projectApi.js
+++ src/config/projectApi.js
@@ -7,9 +7,9 @@ const projectApi = axios.create({
 export default projectApi;
 
 export const setToken = token => {
-  goitApi.defaults.headers.common.Authorization = `Bearer ${token}`;
+  projectApi.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
 
 export const clearToken = () => {
-  goitApi.defaults.headers.common.Authorization = '';
+  projectApi.defaults.headers.common.Authorization = '';
 };
--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,4 +1,4 @@
 export const selectIsAuthenticated = state => state.auth.isAuthenticated;
-export const selectUser = state => state.auth.user.name;
+export const selectUser = state => state.auth.user;
 export const selectAuthError = state => state.auth.error;
 export const selectIsRefreshing = state => state.auth.isRefreshing;
